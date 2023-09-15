919abbc5fc9aa178def196bb0726e46e6b9c423f
Wektor607 <1069035F@mail.ru>
2023-09-15 23:39:45 +0300

    New updates

--- frontend/src/components/UI/InputField.jsx
+++ frontend/src/components/UI/InputField.jsx
@@ -1,25 +1,41 @@
 import React from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css'; // стили для редактора
+import 'react-quill/dist/quill.bubble.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; // стили Bootstrap
 
 function InputField(props) {
-  const formStyle = {
-    marginTop: '8%',
-  };
-
   return (
-    <div className="form-floating" style={formStyle}>
-      <textarea
-        type="text"
-        onChange={props.handleChange}
+    <div className="form-floating" style={{marginTop: "7%"}}>
+      <ReactQuill
+        style={{border: "2px solid black", borderRadius: "5px" }}
         value={props.text}
-        className="form-control"
-        placeholder="Leave a comment here"
-        id="floatingTextarea"
-        style={{whiteSpace: 'pre'}}
+        onChange={props.handleChange}
+        modules={{
+          toolbar: [
+            ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+            ['blockquote', 'code-block'],
+
+            [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+            [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+            [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+            [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+
+            [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+            [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+            [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+            [{ 'font': [] }],
+            [{ 'align': [] }],
+
+            ['clean']         
+          ],
+        }}
+        placeholder={"Введите текст..."}
       />
-      <label htmlFor="floatingTextarea">Введите текст</label>
       <div className="my-3">
-        <button onClick={props.handlePostText} style={{backgroundColor: "#481A65", color: "white"}} type="button" className="btn">Отправить</button>
-        <button onClick={props.handleClear} style={{backgroundColor: "#C39BD3", color: "white"}} type="button" className="btn mx-3">Очистить</button>
+        <button onClick={props.handlePostText} style={{ backgroundColor: "#481A65", color: "white" }} type="button" className="btn">Отправить</button>
+        <button onClick={props.handleClear} style={{ backgroundColor: "#C39BD3", color: "white" }} type="button" className="btn mx-3">Очистить</button>
       </div>
     </div>
   );
